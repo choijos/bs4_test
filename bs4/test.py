0c1207d7c433d4c48061d8e02f96b2e684a347fa
choijos <choijos@uw.edu>
2022-07-21 19:22:36 -0700

    testing

--- bs4/test.py
+++ bs4/test.py
@@ -1,6 +1,6 @@
 from html.parser import HTMLParser
 from bs4 import BeautifulSoup
-import requests
+import requests 
 
 url = "https://www.newegg.com/msi-geforce-rtx-3080-rtx-3080-gaming-z-trio-10g-lhr/p/N82E16814137677?Description=3080&cm_re=3080-_-14-137-677-_-Product"
 
